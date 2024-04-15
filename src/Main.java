a8794d02c94d77136ffedbf4cf752ba4b9e36e04
Виктор Столяров <accordvitek@mail.ru>
2024-04-15 11:12:51 +0300

    add task 01-06

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,54 @@
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+//        Task01
+        System.out.println("Я");
+        System.out.println("хорошо");
+        System.out.println("знаю");
+        System.out.println("Java");
+
+//        Task02
+//        double result1 = (46 + 10) * (10 / 3.0);
+//        int result2 = 29 * 4 * (-15);
+//        System.out.println(result1);
+//        System.out.println(result2);
+
+//        Task03
+//        int number = 10500;
+//        double result = (number / 10.0) / 10;
+//        System.out.println(result);
+
+//        Task04
+//        double a = 3.6;
+//        double b = 4.1;
+//        double c = 5.9;
+//        double result = a * b * c;
+//        System.out.println(result);
+
+//        Task05
+//        Scanner scanner = new Scanner(System.in);
+//        int input1 = scanner.nextInt();
+//        int input2 = scanner.nextInt();
+//        int input3 = scanner.nextInt();
+//        System.out.println(input1);
+//        System.out.println(input2);
+//        System.out.println(input3);
+
+//        Task06
+//        Scanner scanner = new Scanner(System.in);
+//        int input = scanner.nextInt();
+//        if (input % 2 == 0) {
+//            if (input > 100) {
+//                System.out.println("Выход за пределы диапазона");
+//            } else {
+//                System.out.println("Четное");
+//            }
+//        } else {
+//            System.out.println("Нечетное");
+//        }
 
     }
+
 }
